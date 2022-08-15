f4d764378d39d75146cc18c810e99653fe957d69
maximsamus <maximsamus1@gmail.com>
2022-08-15 16:07:29 +0300

    Initial Commit

---
+++ Vegas SwiftUI/Vegas_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vegas_SwiftUIApp.swift
+//  Vegas SwiftUI
+//
+//  Created by Максим Самусь on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Vegas_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

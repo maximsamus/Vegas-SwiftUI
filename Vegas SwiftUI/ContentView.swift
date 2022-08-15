1312dcdb64cb5958b72b8c706c12cfba966d5f20
maximsamus <maximsamus1@gmail.com>
2022-08-15 18:59:45 +0300

    logic fix

--- Vegas SwiftUI/ContentView.swift
+++ Vegas SwiftUI/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image("diceeLogo")
+                HStack {
+                    DiceView(numbers: 1)
+                    DiceView(numbers: 1)
+                }
+                .padding()
+                Button(action: {
+                    
+                }) {
+                    Text("ROLL")
+                        .foregroundColor(.white)
+                        .font(.system(size: 60))
+                        .fontWeight(.bold)
+                }
+
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let numbers: Int
+    var body: some View {
+        Image("dice\(numbers)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
     }
 }
